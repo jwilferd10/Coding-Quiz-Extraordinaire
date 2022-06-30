326384503e9213ed3c5d03a658424bd39f5f17e5
jwilferd <jwilferd10@yahoo.com>
2022-06-30 10:04:41 -0700

    Feat: Refactored the JS, now more uniform. Additional changes likely soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+// When the quiz is over, there is an error at at around line 46 (this is possibly because there's nothing left within the array)
 let questionEl = document.getElementById("question");
 let answerEl = document.getElementById("answers");
 let scoreEl = document.getElementById("results");
-let answer;
 let timerEl = document.getElementById("timer");
-let timeLeft = 60; 
-let questionNumber = -1; // where our questions will start at, set to -1, setting to 0 will skip a question
+let timeLeft = 60;
+let questionNumber = -1; // questions will start at -1, using 0 will skip a question
+
 const testQuestionsArr = [
     { question: "Commonly used data types DO Not Include:", answers: [ "1. strings","2. booleans","3. alerts","4. numbers" ], correctAnswer: "3. alerts" },
     { question: "The condition in an if/else statement is enclosed with ______.", answers: [ "1. quotes","2. curly brackets","3. parenthesis","4. square brackets" ], correctAnswer: "2. curly brackets" },
@@ -13,41 +14,35 @@ const testQuestionsArr = [
     { question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: [ "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"], correctAnswer: "4. console.log" }
 ];
 
-// Everything within here is going to start our timer and display our quiz
-function liveQuiz() {
-    // After starting quiz the intro is hidden
+let startQuiz = function() {
+    // When the quiz is started, hide the intro and display the quiz to users
     document.getElementById("intro").classList.add("hidden");
-    // This removes hidden, showing the quiz
     document.getElementById("quiz").classList.remove("hidden");
-
-    // Starting the timer
+    
     startTimer();
 
-    // Displays the questions to the user
     quizQuestions();
 };
 
-// Everything within this function relates to quiz timer
-function startTimer() {
-        let timer = setInterval(function() {
-        // Ticks down
+let startTimer = function() {
+    let timer = setInterval(function() {
+        // decrement timeLeft
         timeLeft --;
-        // Display visual reminder for user
+
         timerEl.textContent = "Time: " + timeLeft + " seconds remaining";
-        // if statement executes when time has ran out
-        if(timeLeft === 0 || questionNumber === testQuestionsArr.length) {
+
+        if (timeLeft === 0 || questionNumber === testQuestionsArr.length) {
             clearInterval(timer);
             setTimeout(showScore, 200);
         }
     }, 1000);
 };
 
-// This sets the questions and also buttons containing our answers
-function quizQuestions() {
-    
+let quizQuestions = function() {
+    // increment through array
     questionNumber++
 
-    // This presents our question as a textContent to the user.
+    // show's current question
     questionEl.textContent = testQuestionsArr[questionNumber].question
         
     answerEl.innerHTML = "";
@@ -58,15 +53,16 @@ function quizQuestions() {
     // This reaches for our correctAnswer in the array
     answer = testQuestionsArr[questionNumber].correctAnswer;
 
-    // Using a local variable within for, nextAnswer generates buttons our answers
+    // display the array of possible answers
     for (let i = 0; i < answers.length; i++) {
-        // nextAnswer is creating a button element on the document
-        let nextAnswer = document.createElement("button");
-        nextAnswer.className = "potentialAnswer";
-        // nextAnswer is reaching into our array, preparing our answers for next stage
-        nextAnswer.textContent = answers[i];
-        // Remember: This is what appends our button to the webpage
-        answerBtn = answerEl.appendChild(nextAnswer);
+
+        let answerChoices = document.createElement("button");
+
+        answerChoices.className = "potentialAnswer";
+
+        answerChoices.textContent = answers[i];
+
+        answerBtn = answerEl.appendChild(answerChoices);
     }
 };
 
@@ -77,43 +73,34 @@ answerEl.addEventListener("click", function(event) {
     if (answer === event.target.textContent) {
         // Let user know answer is correct
         progressEl.textContent = "Correct Answer!"
-        // Sets the amount of time message is displayed
         setTimeout(hideProgress, 3000);
         showProgress();
     }
     else { 
         // Let user know answer is incorrect
         progressEl.textContent = "Incorrect Answer!";
-        // Sets the amount of time message is displayed
         setTimeout(hideProgress, 3000);
-        // Subtract ten seconds from time left
         timeLeft = timeLeft - 10;
         showProgress();
     }
     quizQuestions();
 });
 
-// progress connects to the html class. This is VERY important in order to 'progress' the quiz!!
-function hideProgress() {
-    let progressEl = document.getElementsByClassName("progress") [0]
-    progressEl.style.display="none";
-};
-
-// When this is called it's removing the none attribute
-function showProgress() {
-    let progressEl = document.getElementsByClassName("progress") [0]
-    progressEl.removeAttribute("style");
-};
-
-// progress connects to the html class. This is VERY important in order to 'progress' the quiz!!
-
-// Once the quiz has been completed, present their results
 let showScore = function() {
     document.getElementById("quiz").classList.add("hidden");
     document.getElementById("results").classList.remove("hidden");
     scoreEl.textContent = "Your final score is: " + timeLeft;
+    
 };
 
-// Button to start counter and quiz
-document.querySelector("#start-btn").addEventListener("click", liveQuiz);
+let hideProgress = function() {
+    let progressEl = document.getElementsByClassName("progress") [0]
+    progressEl.style.display="none";
+};
 
+let showProgress = function() {
+    let progressEl = document.getElementsByClassName("progress") [0]
+    progressEl.removeAttribute("style");
+};
+
+document.querySelector("#start-btn").addEventListener("click", startQuiz);
