93ae7370550f9958dd1346d2a19940e2be6fc7c1
jwilferd <jwilferd10@yahoo.com>
2022-06-30 17:52:51 -0700

    Feat: two buttons successfully appending to page, next stop functionality. Waypoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,10 @@ let questionNumber = -1; // questions will start at -1, using 0 will skip a ques
 
 const testQuestionsArr = [
     { question: "Commonly used data types DO Not Include:", answers: [ "1. strings","2. booleans","3. alerts","4. numbers" ], correctAnswer: "3. alerts" },
-    { question: "The condition in an if/else statement is enclosed with ______.", answers: [ "1. quotes","2. curly brackets","3. parenthesis","4. square brackets" ], correctAnswer: "2. curly brackets" },
-    { question: "Arrays in JavaScript can be used to store", answers: [ "1. numbers and strings","2. other arrays","3. booleans","4. all of the above" ], correctAnswer: "4. all of the above" },
-    { question: "String values must be enclosed within _____ when being assigned to variables.", answers: [ "1. commas","2. curly brackets","3. quotes","4. parenthesis" ], correctAnswer: "3. quotes" },
-    { question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: [ "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"], correctAnswer: "4. console.log" }
+    // { question: "The condition in an if/else statement is enclosed with ______.", answers: [ "1. quotes","2. curly brackets","3. parenthesis","4. square brackets" ], correctAnswer: "2. curly brackets" },
+    // { question: "Arrays in JavaScript can be used to store", answers: [ "1. numbers and strings","2. other arrays","3. booleans","4. all of the above" ], correctAnswer: "4. all of the above" },
+    // { question: "String values must be enclosed within _____ when being assigned to variables.", answers: [ "1. commas","2. curly brackets","3. quotes","4. parenthesis" ], correctAnswer: "3. quotes" },
+    // { question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: [ "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"], correctAnswer: "4. console.log" }
 ];
 
 let startQuiz = function() {
@@ -98,6 +98,19 @@ let showScore = function() {
     let scoreInfo = document.createElement('section');
     scoreInfo.textContent = "Your final score is: " + timeLeft;
     scoreEl.appendChild(scoreInfo);
+
+    // create two buttons, one that returns user back to main page OR take user to enter their high score 
+    let mainPageBtn = document.createElement('button');
+    mainPageBtn.classList.add('btn');
+    mainPageBtn.textContent = "Back To Main Page"
+    scoreEl.appendChild(mainPageBtn);
+
+    let enterScoreBtn = document.createElement('button');
+    enterScoreBtn.classList.add('btn');
+    enterScoreBtn.textContent = "Enter My Score"
+    scoreEl.appendChild(enterScoreBtn);
+
+
 };
 
 let hideProgress = function() {
