e4473c27eb69744dfcfda778e70d72d0dba3d766
jwilferd <jwilferd10@yahoo.com>
2022-09-04 19:05:48 -0700

    Feat: event listener removed from answerEl and added to it's child asset. iterateQuestion does answerEl's previous event

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,10 @@ let userInfoArr = JSON.parse(localStorage.getItem('userInfoArr')) || [];
 // quiz questions array with answer selections and correct answer
 const testQuestionsArr = [
     { question: "Commonly used data types DO Not Include:", answers: [ "1. strings","2. booleans","3. alerts","4. numbers" ], correctAnswer: "3. alerts" },
-    { question: "The condition in an if/else statement is enclosed with ______.", answers: [ "1. quotes","2. curly brackets","3. parenthesis","4. square brackets" ], correctAnswer: "2. curly brackets" },
-    { question: "Arrays in JavaScript can be used to store", answers: [ "1. numbers and strings","2. other arrays","3. booleans","4. all of the above" ], correctAnswer: "4. all of the above" },
-    { question: "String values must be enclosed within _____ when being assigned to variables.", answers: [ "1. commas","2. curly brackets","3. quotes","4. parenthesis" ], correctAnswer: "3. quotes" },
-    { question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: [ "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"], correctAnswer: "4. console.log" }
+    // { question: "The condition in an if/else statement is enclosed with ______.", answers: [ "1. quotes","2. curly brackets","3. parenthesis","4. square brackets" ], correctAnswer: "2. curly brackets" },
+    // { question: "Arrays in JavaScript can be used to store", answers: [ "1. numbers and strings","2. other arrays","3. booleans","4. all of the above" ], correctAnswer: "4. all of the above" },
+    // { question: "String values must be enclosed within _____ when being assigned to variables.", answers: [ "1. commas","2. curly brackets","3. quotes","4. parenthesis" ], correctAnswer: "3. quotes" },
+    // { question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: [ "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"], correctAnswer: "4. console.log" }
 ];
 
 let startQuiz = function() {
@@ -70,11 +70,16 @@ let quizQuestions = function() {
         answerChoices.className = "potentialAnswer";
         answerChoices.textContent = answers[i];
         answerBtn = answerEl.appendChild(answerChoices);
+        answerBtn.addEventListener("click", function(event) {
+            iterateQuestion();
+        })
     }
+    
 };
 
 // connected to the answer buttons generated in the above code
-answerEl.addEventListener("click", function(event) {
+// answerEl.addEventListener("click", function(event) {
+let iterateQuestion = function() {
     // when the button is clicked this furthers users progress
     let progressEl = document.getElementsByClassName("progress") [0]
 
@@ -93,7 +98,7 @@ answerEl.addEventListener("click", function(event) {
     }
     // continue to iterate through next questions
     quizQuestions();
-});
+};
 
 // showProgress displays notification whether answer is right or wrong
 let showProgress = function() {
